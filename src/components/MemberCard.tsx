a3ceb5302b8f95c4bb6fc998a3f7e58bf2c47f30
Jan Ruettinger <service+github@janruettinger.com>
2022-02-20 17:00:31 -0700

    feat: initial commit

---
+++ src/components/MemberCard.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image';
+import * as React from 'react';
+import { useState } from 'react';
+import ReactCardFlip from 'react-card-flip';
+
+type PropType = {
+  fullName: string;
+  flipNote: string;
+  imageName: string;
+};
+
+export default function MemberCard({
+  fullName,
+  flipNote,
+  imageName,
+}: PropType) {
+  // Put Header or Footer Here
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function handleClick(event: { preventDefault: () => void }) {
+    event.preventDefault();
+    setIsFlipped(!isFlipped);
+  }
+
+  return (
+    <div>
+      <ReactCardFlip
+        isFlipped={isFlipped}
+        flipSpeedBackToFront={1}
+        flipSpeedFrontToBack={1}
+      >
+        <div className='h-[400px] w-[400px]' onClick={handleClick}>
+          <Image
+            src={`/images/${imageName}`}
+            alt='Member image'
+            layout='fill'
+            className='rounded-full'
+          />
+        </div>
+
+        <div>
+          <div
+            onClick={handleClick}
+            className='flex h-[400px] w-[400px] flex-col items-center justify-center'
+          >
+            <div>{flipNote}</div>
+          </div>
+        </div>
+      </ReactCardFlip>
+      <div className='text-2xl'>{fullName}</div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,16 @@
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
+import MemberCard from '@/components/MemberCard';
 
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
+const members = [
+  {
+    fullName: 'Jan Ruettinger',
+    flipNote:
+      'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsumlorem ipsum',
+    imageName: 'jan_rue.jpg',
+  },
+];
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
@@ -24,47 +20,28 @@ export default function HomePage() {
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
-      <Seo />
+      {/* <Seo /> */}
 
       <main>
         <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
+          <div className='layout flex flex-col items-center justify-center text-center'>
+            <h1 className='mt-4'>Jessi&apos;s Hacker Crew</h1>
 
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
+            <div className='mt-8 grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-48'>
+              {members.map((member, idx) => {
+                return (
+                  <MemberCard
+                    key={idx}
+                    fullName={member.fullName}
+                    imageName={member.imageName}
+                    flipNote={member.flipNote}
+                  />
+                );
+              })}
+            </div>
 
             <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
+              © {new Date().getFullYear()} By Jessi&apos;s Hackers
             </footer>
           </div>
         </section>
